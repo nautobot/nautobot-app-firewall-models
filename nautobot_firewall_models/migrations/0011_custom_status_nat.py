e575820ada427e3056501096cf5fbf3a0896fc76
Stephen Kiely <stephenkiely87@gmail.com>
2022-10-06 22:58:57 -0500

    Removes unnecessary status content_types.
    This commit fixes a bug that migration 0002 create_status would add all models instead of only the models that support statuses.  A fix of this migration has been provided in addition to a new migration 0012 to cleanup these statuses for anyone already running the plugin.
    
    In addition to these fixes, I am adding reverse_code for all of the RunPython migrations so that when removing this app, you can do `migrate nautobot_firewall_models zero`.

--- nautobot_firewall_models/migrations/0011_custom_status_nat.py
+++ nautobot_firewall_models/migrations/0011_custom_status_nat.py
@@ -3,7 +3,7 @@
 from django.db import migrations
 
 
-def create_nat_status(apps, schedma_editor):
+def create_nat_status(apps, schema_editor):
     """Initial subset of statuses for the NAT models.
 
     This was added along with 0009_nat_policy in order to associate the same set of statuses with the new NAT models
@@ -22,6 +22,24 @@ def create_nat_status(apps, schedma_editor):
             ct = ContentType.objects.get_for_model(model)
             status.content_types.add(ct)
 
+def remove_nat_status(apps, schema_editor):
+    """Remove status content_type for NAT models.
+
+    This was added along with 0009_nat_policy in order to associate the same set of statuses with the new NAT models
+    that are associated with the original set of security models.
+    """
+
+    statuses = ["active", "staged", "decommissioned"]
+    ContentType = apps.get_model("contenttypes.ContentType")
+    relevant_models = [
+        apps.get_model(model)
+        for model in ["nautobot_firewall_models.NATPolicy", "nautobot_firewall_models.NATPolicyRule"]
+    ]
+    for i in statuses:
+        status = apps.get_model("extras.Status").objects.get(slug=i)
+        for model in relevant_models:
+            ct = ContentType.objects.get_for_model(model)
+            status.content_types.remove(ct)
 
 class Migration(migrations.Migration):
 
@@ -31,5 +49,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(code=create_nat_status),
+        migrations.RunPython(code=create_nat_status, reverse_code=remove_nat_status),
     ]
---
+++ nautobot_firewall_models/migrations/0012_remove_status_m2m_through_models.py
@@ -0,0 +1,27 @@
+
+from django.db import migrations
+
+
+def remove_m2m_through_status_content_types(apps, schema_editor):
+    """Remove the through model content types from the Status objects."""
+
+    statuses = ["active", "staged", "decommissioned"]
+    ContentType = apps.get_model("contenttypes.ContentType")
+    for i in statuses:
+        status = apps.get_model("extras.Status").objects.get(slug=i)
+        for model in apps.app_configs["nautobot_firewall_models"].get_models():
+            if not hasattr(model, "status"):
+                ct = ContentType.objects.get_for_model(model)
+                status.content_types.remove(ct)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("extras", "0033_add__optimized_indexing"),
+        ("nautobot_firewall_models", "0011_custom_status_nat"),
+    ]
+
+    operations = [
+        migrations.RunPython(code=remove_m2m_through_status_content_types, reverse_code=migrations.RunPython.noop),
+    ]
