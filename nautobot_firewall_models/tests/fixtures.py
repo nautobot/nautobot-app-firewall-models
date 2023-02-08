1f17af7cd31943f81e7eb7d362779c726f359dcd
Jeremy White <jeremy.lee.w@me.com>
2023-02-08 14:51:26 -0600

    rerun black

--- nautobot_firewall_models/tests/fixtures.py
+++ nautobot_firewall_models/tests/fixtures.py
@@ -83,7 +83,7 @@ def create_env():
         default_ip_protocol="TCP",
         status=status,
         risk=3,
-        description="some description"
+        description="some description",
     )
     app2 = ApplicationObject.objects.create(
         name="app2",
@@ -93,7 +93,7 @@ def create_env():
         default_ip_protocol="TCP",
         status=status,
         risk=2,
-        description="some description"
+        description="some description",
     )
     app3 = ApplicationObject.objects.create(
         name="app3",
@@ -103,7 +103,7 @@ def create_env():
         default_ip_protocol="TCP",
         status=status,
         risk=1,
-        description="some description"
+        description="some description",
     )
     app_grp1 = ApplicationObjectGroup.objects.create(name="streaming", description="some description")
     app_grp1.application_objects.set([app1])
@@ -113,7 +113,13 @@ def create_env():
     app_grp3.application_objects.set([app1, app2, app3])
 
     pol_rule1 = PolicyRule.objects.create(
-        action="deny", log=True, name="Policy Rule 1", status=status, request_id="req1", index=10, description="some description"
+        action="deny",
+        log=True,
+        name="Policy Rule 1",
+        status=status,
+        request_id="req1",
+        index=10,
+        description="some description",
     )
     pol_rule1.source_users.set([usr_obj1])
     pol_rule1.source_user_groups.set([usr_grp1])
@@ -134,7 +140,8 @@ def create_env():
         name="Policy Rule 2",
         status=status,
         request_id="req2",
-        index=20, description="some description"
+        index=20,
+        description="some description",
     )
     pol_rule2.source_users.set([usr_obj1, usr_obj2])
     pol_rule2.source_user_groups.set([usr_grp1, usr_grp2])
@@ -154,7 +161,8 @@ def create_env():
         name="Policy Rule 3",
         status=status,
         request_id="req3",
-        index=30, description="some description"
+        index=30,
+        description="some description",
     )
     pol_rule3.source_users.set([usr_obj1, usr_obj2, usr_obj3])
     pol_rule3.source_user_groups.set([usr_grp1, usr_grp2, usr_grp3])
