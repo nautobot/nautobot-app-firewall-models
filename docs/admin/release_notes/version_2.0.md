6ec8dd8a64b015bab0b3427091dd987e7dbf0056
Jeremy White <jeremy.lee.w@me.com>
2024-01-04 10:17:12 -0600

    Closes: #199 - incorrect reverse in template

--- docs/admin/release_notes/version_2.0.md
+++ docs/admin/release_notes/version_2.0.md
@@ -2,6 +2,12 @@
 
 This document describes all new features and changes in the release `2.0`. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## v2.0.2 - 2024-01-04
+
+### Fixed
+
+- [#199](https://github.com/nautobot/nautobot-plugin-firewall-models/issues/199) Resolve incorrect reverse for custom viewset
+
 ## v2.0.1 - 2023-10-04
 
 ### Fixed
--- nautobot_firewall_models/tests/test_ui_views.py
+++ nautobot_firewall_models/tests/test_ui_views.py
@@ -2,6 +2,7 @@
 # flake8: noqa: F403,405
 # pylint: disable=invalid-name
 # pylint: disable=duplicate-code
+from nautobot.dcim.models import Device
 from nautobot.extras.models.statuses import Status
 from nautobot.apps.testing import ViewTestCases
 
@@ -384,3 +385,16 @@ class NATPolicyUIViewTest(ViewTestCases.PrimaryObjectViewTestCase):
             f'csvrule2,"{nat_pol_rule.id}",Active',
             f'csvrule3,"{nat_pol_rule.id}",Active',
         )
+
+
+class CapircaPolicyUIViewTest(ViewTestCases.GetObjectViewTestCase, ViewTestCases.ListObjectsViewTestCase):
+    """Test the Policy viewsets."""
+
+    model = CapircaPolicy
+
+    @classmethod
+    def setUpTestData(cls):
+        """Create test data."""
+        fixtures.create_capirca_env()
+        for device in Device.objects.all():
+            CapircaPolicy.objects.create(device=device)
